1a3ca295d7fa96af7f2bbe0f75682fc4ab7ef7cc
Francisco José García Cutillas <3979199@alu.murciaeduca.es>
2024-05-13 21:23:40 +0200

    Implementado Get horarios por id

--- src/main/java/com/Guardias/controller/HorarioController.java
+++ src/main/java/com/Guardias/controller/HorarioController.java
@@ -12,7 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +46,34 @@ public class HorarioController {
         }
         
         return new ResponseEntity<>(horariosDevolver, HttpStatus.OK);
+    }
+    
+    @GetMapping("/horarios/{id}")
+    public ResponseEntity<HorarioDto> getHorarioById(Integer id){
+        
+        Horario horario = hService.consultar(id);
+        
+        if(horario.getIdSesion() == null){
+            
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            
+        }
+        
+        HorarioDto horarioDevolver = HorarioDto.toHorarioDto(horario);
+        
+        return new ResponseEntity<>(horarioDevolver, HttpStatus.OK);
+        
+    }
+    
+//    @PostMapping
+//    public ResponseEntity<HorarioDto> postHorario(@RequestBody HorarioDto horarioDto){
+//        
+//        
+//        
+//    }
+    
+    @DeleteMapping()
+    public void deleteHorario(Integer id){
+        hService.eliminar(id);
     }
 }
